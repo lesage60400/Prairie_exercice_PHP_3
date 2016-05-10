0b5c08f045b0dd1d53dfe07dcc7ed9012c584a8b
lesage60400 <lesage.pierre@neuf.fr>
2016-05-10 09:34:05 +0200

    Exercice 3 prairie PHP du 09/05/2016

---
+++ exercice3php.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Nombre impair entre 1 et 300 exercice 3 prairie du 09/05/2016</title>
+  </head>
+  <body>
+    <?php
+    $min=1;
+    $max=300;
+      for ($i=$min; $i<$max; $i = $i + 2) {
+        echo "<p>$i</p>";
+      }
+    ?>
+  </body>
+</html>
